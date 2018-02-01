6f49063d03ce7bd760c829c0ae9a6512c31f5f68
Matt Braden <matt.braden73@gmail.com>
2018-01-31 20:13:58 -0500

    I added basic functionality of card creation

---
+++ Assets/Scripts/Card Stuff/CardReader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardReader : MonoBehaviour {
+    [SerializeField]
+    Card myCard;
+    void Start () {
+        if (myCard != null)
+        {
+            foreach (Transform obj in gameObject.GetComponentsInChildren<Transform>())
+                if (obj.gameObject.tag.Equals("CardArt")) obj.gameObject.GetComponent<Image>().sprite = myCard.artwork;
+            foreach (Transform obj in gameObject.GetComponentsInChildren<Transform>())
+                if (obj.gameObject.tag.Equals("CardName")) obj.gameObject.GetComponent<Text>().text = myCard.cardName;
+            foreach (Transform obj in gameObject.GetComponentsInChildren<Transform>())
+                if (obj.gameObject.tag.Equals("CardDescription")) obj.gameObject.GetComponent<Text>().text = myCard.description;
+        }
+    }
+}
