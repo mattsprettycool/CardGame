87a37c895f9eed8bfd20505b5f1cf658935da8bf
jaisaka <jaitheboss1@gmail.com>
2018-02-21 12:23:30 -0500

    fuck

--- Assets/Scripts/Hand Stuff/HandScript.cs
+++ Assets/Scripts/Hand Stuff/HandScript.cs
@@ -1,18 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HandScript : MonoBehaviour {
 	public GameObject posToPutCard;
+	float timer;
+	int numCards;
 	// Use this for initialization
 	void Start () {
-		for (int i = 0; i < 6; i++) {
-			Instantiate (posToPutCard, new Vector2 (transform.localPosition.x - 640 + (gameObject.GetComponent<RectTransform>().rect.width), 0), transform.localRotation);
-		}
+		numCards = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		timer += Time.deltaTime;
+		if (timer > 2) {
+			DrawCard ();
+			timer = 0;
+		}
+	}
+	public void DrawCard(){
+		numCards++;
+		Instantiate (posToPutCard, gameObject.transform);
 	}
 }
