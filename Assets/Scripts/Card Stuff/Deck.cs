bf0d0ccfc2fbcaeb01cf18067ac28f408a947f5b
HuntoToast <33074737+HuntoToast@users.noreply.github.com>
2018-02-27 15:38:23 -0500

    deck stuff

---
+++ Assets/Scripts/Card Stuff/Deck.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deck : MonoBehaviour {
+
+	public ArrayList deck = new ArrayList();
+    private Random randy = new Random();
+
+    public void shuffle()
+    {
+        ArrayList shuffled = new ArrayList();
+        int count = 0;
+        for(int i = deck.Count; i > 0; i--)
+        {
+            float rnd = Random.Range(0, deck.Count);
+            shuffled.Add(deck[(int)rnd]);
+            deck.Remove(rnd);
+        }
+    }
+
+    public ArrayList createDeck(ArrayList d)
+    {
+        foreach(Card obj in d)
+        {
+            deck.Add(d);
+        }
+        return deck;
+    }
+
+}
