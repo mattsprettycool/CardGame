725ca6e96e899a9de1490187af625ee47641d35c
Matt Braden <matt.braden73@gmail.com>
2018-02-27 15:31:18 -0500

    i effected

---
+++ Assets/Scripts/Card Stuff/EffectManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectManager : MonoBehaviour {
+    [SerializeField]
+    Card c;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.E))
+            DoEffect(c, 0, 0);
+	}
+    
+    void DoEffect(Card c, int x, int y)
+    {
+        if (c.encodedEffectArray[0, 0] == 1)
+            Debug.Log("you dealt damage");
+    }
+}
