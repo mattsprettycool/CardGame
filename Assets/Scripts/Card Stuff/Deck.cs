6c39c16da40bf0978d114988f28eb257dbb35e83
HuntoToast <33074737+HuntoToast@users.noreply.github.com>
2018-03-02 13:26:30 -0500

    DSGDSDFG

--- Assets/Scripts/Card Stuff/Deck.cs
+++ Assets/Scripts/Card Stuff/Deck.cs
@@ -7,6 +7,8 @@ public class Deck : MonoBehaviour {
 
 	public Card[] deck = new Card[100];
     public Card[] discard = new Card[100];
+    public Card[] exhaust = new Card[25];
+    public int exhaustIndex;
     private Random randy = new Random();
     //Maybe this shuffles the deck? Maybe it doesnt
     public void shuffle()
@@ -24,9 +26,9 @@ public class Deck : MonoBehaviour {
             {
                 deck[i] = shuffled[i];
             }
-        }//else if(Effect == shuffle) then run the same code, probably just use an or at the top but tell me about effects matt
+        }
     }
-
+    //Checks if the deck has only null spots or not
     public bool IsEmpty()
     {
         bool me = false;
@@ -43,31 +45,46 @@ public class Deck : MonoBehaviour {
         }
         return me;
     }
-
+    //Takes a card from the deck array and gets rid of it to the discard
     public Card AddAndRemoveCard(int indx)
     {
         Card draw;
         draw = deck[indx];
                 for(int j = indx; j < deck.Length; j++)
                 {
-                    deck[j] = deck[j + 1];
-                    if(deck[j+2] == null)
+                    if(j > 97)
+                    {
+                        deck[98] = deck[99];
+                        deck[99] = null;
+                    }
+                    else if(deck[j+2] == null)
                     {
                         deck[j + 1] = null;
                     }
+                    else
+                    {
+                        deck[j] = deck[j + 1];
+            }
                 }
         return draw;
+    }
+    //tell me if you want this to be void or return a card or something
+    public void Exhaust(Card ex)
+    {
+        Card dis = ex;
+        exhaust[exhaustIndex] = dis;
+        exhaustIndex++;
+        //ill add something where if a new game is started then index is reset but not needed for now
     }
 
     public void Start()
     {
-        //i need help is this how i do it
-        shuffle();
+        
     }
 
     public void FixedUpdate()
     {
-        
+        AddAndRemoveCard(0);
     }
 
 }
