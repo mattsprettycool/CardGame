77cbed5b7668d74dee74125eeddca34fc41565ce
Matt Braden <matt.braden73@gmail.com>
2018-03-20 15:26:00 -0400

    PUSH

--- Assets/Scripts/Card Stuff/EffectManager.cs
+++ Assets/Scripts/Card Stuff/EffectManager.cs
@@ -12,45 +12,44 @@ public class EffectManager : MonoBehaviour {
     //0 = none, 1 = numerical damage, 2 = material damage, 3 = draw, 4 = heal
     void DoEffect(Card c, int x, int y)
     {
-        //int[,] testEffect = { { 1, 2 }, { 2, 4 }, { 3, 1 } };
         for(int i = 0; i < c.effectTypeArr.GetLength(0); i++)
-        {//NEEDS 2 MODS
+        {
             switch (c.effectTypeArr[i])
             {
                 case 0:
                     break;
                 case 1:
-                    DoNumericalDamage(c.mod1Arr[i], x, y);
+                    DoNumericalDamage(c.mod1Arr[i], c.mod2Arr[i], x, y);
                     break;
                 case 2:
-                    DoMaterialBasedDamage(c.mod1Arr[i], x, y);
+                    DoMaterialBasedDamage(c.mod1Arr[i], c.mod2Arr[i], x, y);
                     break;
                 case 3:
-                    DoDraw(c.mod1Arr[i], x, y);
+                    DoDraw(c.mod1Arr[i], c.mod2Arr[i], x, y);
                     break;
                 case 4:
-                    DoHeal(c.mod1Arr[i], x, y);
+                    DoHeal(c.mod1Arr[i], c.mod2Arr[i], x, y);
                     break;
             }
         }
     }
 
-    void DoNumericalDamage(int mod, int x, int y)
+    void DoNumericalDamage(int mod, int mod2, int x, int y)
     {
         Debug.Log("you dealt " + mod + " damage");
     }
 
-    void DoMaterialBasedDamage(int mod, int x, int y)
+    void DoMaterialBasedDamage(int mod, int mod2, int x, int y)
     {
         Debug.Log("you dealt " + mod + " material damage");
     }
 
-    void DoDraw(int mod, int x, int y)
+    void DoDraw(int mod, int mod2, int x, int y)
     {
         Debug.Log("you drew " + mod + " card(s)");
     }
 
-    void DoHeal(int mod, int x, int y)
+    void DoHeal(int mod, int mod2, int x, int y)
     {
         Debug.Log("you healed " + mod + " health");
     }
